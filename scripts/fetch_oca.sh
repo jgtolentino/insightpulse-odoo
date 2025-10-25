658ab822921aad3fdb05156a5a91436699272dd4
Jake Tolentino <jgtolentino_rn@yahoo.com>
2025-10-25 14:53:44 +0800

    chore(oca): vendor OCA repositories

--- scripts/fetch_oca.sh
+++ scripts/fetch_oca.sh
@@ -13,11 +13,20 @@ clone_repo() {
 # Core repos
 clone_repo server-tools
 clone_repo server-auth
+clone_repo server-env
+clone_repo server-backend
 clone_repo web
 clone_repo queue
+clone_repo rest-framework
+clone_repo account-budgeting
+clone_repo account-financial-reporting
 clone_repo account-financial-tools
-clone_repo reporting-engine
+clone_repo dms
 clone_repo hr
+clone_repo hr-expense
+clone_repo project
 clone_repo purchase-workflow
+clone_repo reporting-engine
+clone_repo sale-workflow
 
 echo "Fetched OCA repos. Enable needed modules in Apps."
