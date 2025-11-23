ba64b77fb5fcb514bf97428b1aa0cbf62ec3d3be
Jake Tolentino <jgtolentino_rn@yahoo.com>
2025-11-23 11:55:00 +0800

    fix(tbwa-spectra): Add missing data files to manifest
    - Added security/tbwa_security.xml (security groups and record rules)
    - Added data/users_data.xml (placeholder users and employees)
    - Added data/tbwa_cron.xml (4 automated cron jobs)
    - Maintained correct loading order: security → data → views → wizards
    - Commented out non-existent files with TODO markers
    
    All referenced files verified to exist.

--- addons/tbwa_spectra_integration/__manifest__.py
+++ addons/tbwa_spectra_integration/__manifest__.py
@@ -13,7 +13,23 @@
         "hr_expense",
     ],
     "data": [
+        # Security (must load first)
+        "security/tbwa_security.xml",
         "security/ir.model.access.csv",
+        # Data files (load after security, before views)
+        "data/users_data.xml",
+        "data/tbwa_cron.xml",
+        # 'data/spectra_mapping_data.xml',  # TODO: Create
+        # 'data/approval_matrix_data.xml',  # TODO: Create
+        # 'data/export_templates_data.xml',  # TODO: Create
+        # Views (load last, comment out until created)
+        # 'views/hr_expense_advance_views.xml',  # TODO: Create
+        # 'views/spectra_export_views.xml',  # TODO: Create
+        # 'views/spectra_mapping_views.xml',  # TODO: Create
+        # 'views/approval_matrix_views.xml',  # TODO: Create
+        # 'views/tbwa_menu.xml',  # TODO: Create
+        # Wizards (load after views)
+        # 'wizards/spectra_export_wizard_views.xml',  # TODO: Create
     ],
     "installable": True,
     "application": False,
